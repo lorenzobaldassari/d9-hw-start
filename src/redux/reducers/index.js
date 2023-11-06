6489d9ca8dc351157d89993738e13413dfa097a7
Lorenzo Baldassari <lorebalda93@gmail.com>
2023-11-06 16:46:08 +0100

    first commit

---
+++ src/redux/reducers/index.js
@@ -0,0 +1,34 @@
+const initialState = {
+  favourites: {
+    content: [],
+  },
+};
+
+const mainReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD_TO_FAVOURITES":
+      return {
+        ...state,
+        favourites: {
+          ...state.favourites,
+          content: [...state.favourites.content, action.payload],
+        },
+      };
+    case "REMOVE_FROM_FAVOURITES":
+      return {
+        ...state,
+        favourites: {
+          ...state.favourites,
+          content: [
+            ...state.favourites.content.filter((cont) => {
+              return cont._id !== action.payload;
+            }),
+          ],
+        },
+      };
+    default:
+      return state;
+  }
+};
+
+export default mainReducer;
